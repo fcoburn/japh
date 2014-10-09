13875ec9c9aceb590dd0f59da210a49f6485c3d4
Frank Coburn <fcoburn@gmail.com>
2014-10-09 17:18:55 -0400

    first japh

---
+++ japh.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+
+my @c = ('j','u','s','t','a','n','o','h','e','r','p','l','c','k',' ');
+print "$c[0]$c[1]$c[2]$c[3]$c[scalar@c-1]$c[4]$c[5]$c[6]$c[3]$c[7]$c[8]$c[9]$c[scalar@c-1]$c[10]$c[8]$c[9]$c[11]$c[scalar@c-1]$c[7]$c[4]$c[12]$c[scalar@c-2]$c[scalar@c-7]$c[scalar@c-6]";
